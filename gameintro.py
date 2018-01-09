f8962913b51ca4f59dff129a51acb9452f73484e
SofiaFrias <sfrias@ucvts.tec.nj.us>
2018-01-09 17:11:59 +0000

    initial commit

---
+++ gameintro.py
@@ -0,0 +1,8 @@
+print("Welcome Frozen Python Mountain!  BRRRRRRR it’s the coldest day of the year.  I hope you packed your mittens!  The mountain has 35 slopes lined with aspens and pines.  Right now, we’re about halfway to the top of the mountain.  From the summit (at a whopping 10,000 feet above sea level), enjoy a breathtaking view!")
+lift = input("Would you like to take the high speed lift to the top?")
+if lift == "yes":
+    print("Oh no! It seems that you have lost your lift ticket.")
+else:
+    print("Ok, have fun chilling out down here!")
+while lift == "yes":
+
